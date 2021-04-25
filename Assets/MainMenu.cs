01a9866e1ecb40c9b4f6636da86a33cb87a7a0fb
Michael Wahba <mwahba@ucalgary.ca>
2021-04-25 12:44:32 -0600

    main menu skeleton done

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject CreditsPane;
+
+
+    private void Start()
+    {
+        CreditsPane.SetActive(false);
+    }
+
+    public void OnPlayButtonPress()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OnCreditsButtonPress()
+    {
+        CreditsPane.SetActive(true);
+    }
+    
+    public void OnCreditsHidePress()
+    {
+        CreditsPane.SetActive(false);
+    }
+
+
+    public void OnQuitButtonPress()
+    {
+        Application.Quit();
+    }
+}
