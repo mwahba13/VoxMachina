0239d4064c9b0050cc148dd04cbfdf2dc4493297
Michael Wahba <mwahba@ucalgary.ca>
2021-04-25 13:16:29 -0600

    pause menu

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject pauseMenuObj;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                //Cursor.visible = true;
+                Time.timeScale = 0;
+                pauseMenuObj.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                Debug.Log("Return to game");
+                //Cursor.visible = false;
+
+                pauseMenuObj.SetActive(false);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && Time.timeScale == 0)
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+
+    public void OnReturnToGameButtonPress()
+    {
+
+    }
+}
