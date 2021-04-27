aae2103f6f15fafe7f7107bfd29fafcab36f76bb
Michael Wahba <mwahba@ucalgary.ca>
2021-04-26 18:31:23 -0600

    add images

---
+++ Assets/Scripts/ScriptableObjs/PlayerSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "PlayerSettings", menuName = "PlayerSettings", order = 0)]
+    public class PlayerSettings : ScriptableObject
+    {
+        public bool isInvertY;
+    }
+}
